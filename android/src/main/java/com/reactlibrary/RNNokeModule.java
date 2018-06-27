ad5e70eb70472bc562f47f80de2940a87123e61f
Arda Ilgaz <ardailgaz@hotmail.com>
2018-06-27 16:58:14 +0200

    Java module continued

--- android/src/main/java/com/reactlibrary/RNNokeModule.java
+++ android/src/main/java/com/reactlibrary/RNNokeModule.java
@@ -4,48 +4,62 @@ package com.reactlibrary;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.LifecycleEventListener;
+
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.Context;
 import android.os.IBinder;
 import android.content.ComponentName;
+import android.util.Log;
+import android.support.annotation.Nullable;
+import android.os.Bundle;
 
 import com.noke.nokemobilelibrary.NokeDevice;
 import com.noke.nokemobilelibrary.NokeServiceListener;
 import com.noke.nokemobilelibrary.NokeDeviceManagerService;
 
-public class RNNokeModule extends ReactContextBaseJavaModule {
-
+public class RNNokeModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
     private final ReactApplicationContext reactContext;
+    private NokeDeviceManagerService mNokeService = null;
+    private NokeDevice currentNoke;
 
     public RNNokeModule(ReactApplicationContext reactContext) {
         super(reactContext);
-        this.reactContext = reactContext;
+        this.reactContext=reactContext;
     }
-
     @Override
     public String getName() {
         return "RNNoke";
     }
 
-    private NokeDeviceManagerService mNokeService = null;
-    private NokeDevice currentNoke;
-
     private void initiateNokeService() {
-        Intent nokeServiceIntent = new Intent(reactContext, NokeDeviceManagerService.class);
+        Intent nokeServiceIntent = new Intent(getReactApplicationContext(), NokeDeviceManagerService.class);
         reactContext.bindService(nokeServiceIntent, mServiceConnection, Context.BIND_AUTO_CREATE);
     }
     private ServiceConnection mServiceConnection = new ServiceConnection() {
         public void onServiceConnected(ComponentName className, IBinder rawBinder) {
+            Log.e("Info: ", "serviceConnected");
             //Store reference to service
             mNokeService = ((NokeDeviceManagerService.LocalBinder) rawBinder).getService();
+            //Uncomment to allow devices that aren't in the device array
+            //mNokeService.setAllowAllDevices(true);
             //Register callback listener
             mNokeService.registerNokeListener(mNokeServiceListener);
+            Log.e("Info: ", "nokeListenerRegistered");
+            //Add locks to device manager
+            mNokeService.addNokeDevice(new NokeDevice("NOKE", "DA:AD:B3:5A:EE:9B"));
+            mNokeService.addNokeDevice(new NokeDevice("NOKE", "F6:C8:C3:E0:A7:7D"));
+            Log.e("Info: ", "nokeDevicesAdded");
             //Start bluetooth scanning
             mNokeService.startScanningForNokeDevices();
+            Log.e("MyTagGoesHere", "mNokeService.startScanningForNokeDevices()");
+            sendEvent(reactContext,"initDone","dwdw");
             if (!mNokeService.initialize()) {
-                //Log.e(TAG, "Unable to initialize Bluetooth");
+                //sendEvent(reactContext,"initDone","dwdw");
+                Log.e("E", "Unable to initialize Bluetooth");
             }
         }
         public void onServiceDisconnected(ComponentName classname) {
@@ -55,45 +69,46 @@ public class RNNokeModule extends ReactContextBaseJavaModule {
     private NokeServiceListener mNokeServiceListener = new NokeServiceListener() {
         @Override
         public void onNokeDiscovered(NokeDevice noke) {
-
+            Log.e("Info: ", "onNokeDiscovered");
         }
-
         @Override
         public void onNokeConnecting(NokeDevice noke) {
-
+            Log.e("Info: ", "onNokeConnecting");
         }
-
         @Override
         public void onNokeConnected(NokeDevice noke) {
-
+            Log.e("Info: ", "onNokeConnected");
         }
-
         @Override
         public void onNokeSyncing(NokeDevice noke) {
-
+            Log.e("Info: ", "onNokeSyncing");
         }
-
         @Override
         public void onNokeUnlocked(NokeDevice noke) {
-
+            Log.e("Info: ", "onNokeUnlocked");
         }
         @Override
         public void onNokeDisconnected(NokeDevice noke) {
-
+            Log.e("Info: ", "onNokeDisconnected");
         }
         @Override
         public void onBluetoothStatusChanged(int bluetoothStatus) {
-
+            Log.e("Info: ", "onBluetoothStatusChanged");
         }
         @Override
         public void onError(NokeDevice noke, int error, String message) {
-            // Log.e(TAG, "NOKE SERVICE ERROR " + error + ": " + message);
+            Log.e("Info: ", "NOKE SERVICE ERROR " + error + ": " + message);
         }
     };
 
     @ReactMethod
     public void initNoke() {
-      initiateNokeService();
+        initiateNokeService();
+    }
+    private void sendEvent(ReactContext reactContext, String eventName, @Nullable String param) {
+        reactContext
+            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+            .emit(eventName, param);
     }
     // @ReactMethod
     // public void onUnlockReceived(String response, NokeDevice noke) {
@@ -112,4 +127,19 @@ public class RNNokeModule extends ReactContextBaseJavaModule {
     //    //     //Log.e(TAG, e.toString());
     //    // }
     // }
+    @Override
+    public void onHostResume() {
+        getReactApplicationContext()
+            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+            .emit("activityResume", null);
+    }
+    @Override
+    public void onHostPause() {
+        getReactApplicationContext()
+            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+            .emit("activityPause", null);
+    }
+    @Override
+    public void onHostDestroy() {
+    }
 }
